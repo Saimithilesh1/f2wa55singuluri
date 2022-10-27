37037d4cb069b125eb1a8301f184540f22b3a2d4
Saimithilesh1 <saimithilesh111@gmail.com>
2022-10-26 19:55:44 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,48 +1,20 @@
 var express = require('express');
-
 var router = express.Router();
-
-
-
 router.get('/', function(req, res, next) {
-
   var NR1 = Math.random() *10;
-
   var NR2 = Math.random() *10;
-
   var NR3 = Math.random() *10;
-
-
   var COS = Math.cos(NR1);
-
   var SIN = Math.asin(NR2);
-
   var SINH = Math.asinh(NR3);
-
-  
-
   res.render('computation', { title: 'Sai Mithilesh Singuluri',
-
   NR1:NR1,
-
   NR2:NR2,
-
   NR3:NR3,
-
-
-
   COS:COS,
-
   SIN:SIN,
-
   SINH:SINH,
-
- 
-
 });
-
 });
-
-
 
 module.exports = router;
